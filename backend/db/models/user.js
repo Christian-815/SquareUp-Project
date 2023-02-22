d4e4c22046efbe220f226095e63df0e6d3ff545f
Christian Oviedo <Chris_ovi1@hotmail.com>
2023-02-21 22:43:40 -0800

    Fixed realtionship issue

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -47,7 +47,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.hasMany(models.Group, {foreignKey: 'organizerId'});
-      User.belongsToMany(models.Event, { through: models.Attendance });
+      User.hasMany(models.Attendance, {foreignKey: 'userId'});
       User.belongsToMany(models.Group, { through: models.Membership });
     }
   };
